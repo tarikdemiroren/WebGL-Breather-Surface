34ca981d81c11a647ded09c4d25f50607e467cbc
Tarık Demirören <tarekarez5652@gmail.com>
2023-12-11 19:16:18 +0300

    html moved to the javascript file

--- breather.js
+++ breather.js
@@ -1,175 +1,208 @@
-function initShaders(gl) {
-    const vertexShaderSource = `
-    attribute vec4 aVertexPosition;
-    attribute vec3 aVertexNormal;
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
-    uniform float uTime;
-    varying vec3 vNormal;
+document.addEventListener("DOMContentLoaded", function () {
 
-    void main(void) {
-        gl_Position = aVertexPosition;
+    let uRangeStart;
+    let uRangeEnd;
+    let vRangeStart;
+    let vRangeEnd;
+
+    let uRange = [];
+    let vRange = [];
+    let uPrecision = 0.05;
+    let vPrecision = 0.05;
+    let aa = 0.5;
+
+    document.getElementById("button").addEventListener("click", getNumbers);
+
+    const uPrecisionValueSpan = document.getElementById('uPrecisionValue');
+    uPrecisionValueSpan.textContent = `${uPrecision}`;
+
+    document.getElementById("uPrecision").addEventListener("input", function () {
+        uPrecision = this.value;
+        uPrecisionValueSpan.textContent = `${uPrecision}`;
+      });
+
+    
+      const vPrecisionValueSpan = document.getElementById('vPrecisionValue');
+      vPrecisionValueSpan.textContent = `${vPrecision}`;
+
+      document.getElementById("vPrecision").addEventListener("input", function () {
+        vPrecision = this.value;
+        vPrecisionValueSpan.textContent = `${vPrecision}`;
+      });
+
+      const aaPrecisionValueSpan = document.getElementById('aaPrecisionValue');
+    aaPrecisionValueSpan.textContent = `${aa}`;
+
+      document.getElementById("aaValue").addEventListener("input", function () {
+        aa = this.value;
+        aaPrecisionValueSpan.textContent = `${aa}`;
+      });
+
+    function getNumbers() {
+        const uRangeStartInput = document.getElementById("uRangeStart").value;
+        const uRangeEndInput = document.getElementById("uRangeEnd").value;
+        const vRangeStartInput = document.getElementById("vRangeStart").value;
+        const vRangeEndInput = document.getElementById("vRangeEnd").value;
+      
+      
+        // Convert inputs to integers
+        uRangeStart = parseInt(uRangeStartInput);
+        uRangeEnd = parseInt(uRangeEndInput);
+        vRangeStart = parseInt(vRangeStartInput);
+        vRangeEnd = parseInt(vRangeEndInput);
+
+        uRange = [uRangeStart, uRangeEnd];
+        vRange = [vRangeStart, vRangeEnd];
+      }
+
+    const canvas = document.getElementById("webgl-canvas");
+    const gl = canvas.getContext("webgl");
+
+    if (!gl) {
+        console.error("Unable to initialize WebGL. Your browser may not support it.");
+        return;
     }
 
-    // void main(void) {
-    //     // Create a breathing effect using a sine function
-    //     float amplitude = 0.5; // Adjust the amplitude as needed
-    //     float frequency = 2.0; // Adjust the frequency as needed
-    //     float displacement = amplitude * sin(uTime * frequency);
-
-    //     // Displace the vertex along the normal direction
-    //     vec4 displacedPosition = aVertexPosition + vec4(aVertexNormal * displacement, 0.0);
-
-    //     // Transform the vertex position
-    //     gl_Position = uProjectionMatrix * uModelViewMatrix * displacedPosition;
-
-    //     // Pass the normal to the fragment shader for lighting calculations
-    //     vNormal = mat3(uModelViewMatrix) * aVertexNormal;
-    // }
+    // Vertex and fragment shader source code
+    const vertexShaderSource = `
+        attribute vec4 a_position;
+        uniform mat4 u_matrix;
+        void main() {
+            gl_Position = u_matrix * a_position;
+        }
     `;
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderSource);
-    gl.compileShader(vertexShader);
 
     const fragmentShaderSource = `
-    precision mediump float;
-    varying vec3 vNormal;
-
-    void main(void) {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
-    }
-
-    // void main(void) {
-    //     // Normalize the normal vector to ensure accurate lighting calculations
-    //     vec3 normal = normalize(vNormal);
-
-    //     // Use the normal vector to determine color (example: gradient based on Y-axis)
-    //     vec3 color = vec3(0.5, 0.5, 0.5) + 0.5 * normal;
-
-    //     // Output the final color
-    //     gl_FragColor = vec4(color, 1.0);
-    // }
+        precision mediump float;
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
+        }
     `;
 
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderSource);
-    gl.compileShader(fragmentShader);
+    // Compile shaders
+    function compileShader(type, source) {
+        const shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
 
-    var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            console.error(`Shader compilation error: ${gl.getShaderInfoLog(shader)}`);
+            gl.deleteShader(shader);
+            return null;
+        }
 
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        console.error('Unable to initialize the shader program:', gl.getProgramInfoLog(shaderProgram));
-        return null;
-    }
-    
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.error('Vertex shader compilation error:', gl.getShaderInfoLog(vertexShader));
-        return null;
-    }
-    
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.error('Fragment shader compilation error:', gl.getShaderInfoLog(fragmentShader));
-        return null;
-    }    
-
-    return shaderProgram;
-}
-
-var canvas;
-var gl;
-var program;
-
-var projectionMatrix;
-var modelViewMatrix;
-
-var vertices;
-var normals;
-
-// Example vertices for a flat plane
-vertices = [
-    vec4(-1.0, 0.0, -1.0, 1.0),  // Vertex 1
-    vec4(1.0, 0.0, -1.0, 1.0),   // Vertex 2
-    vec4(-1.0, 0.0, 1.0, 1.0),   // Vertex 3
-    vec4(1.0, 0.0, 1.0, 1.0)     // Vertex 4
-];
-
-// Example normal vectors for the flat plane (pointing upward)
-normals = [
-    normalize(vec3(0.0, 1.0, 0.0)),  // Normal for Vertex 1
-    normalize(vec3(0.0, 1.0, 0.0)),  // Normal for Vertex 2
-    normalize(vec3(0.0, 1.0, 0.0)),  // Normal for Vertex 3
-    normalize(vec3(0.0, 1.0, 0.0))   // Normal for Vertex 4
-];
-
-
-window.onload = function init() {
-    canvas = document.getElementById("gl-canvas");
-
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) {
-        alert("WebGL isn't available");
+        return shader;
     }
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    const vertexShader = compileShader(gl.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = compileShader(gl.FRAGMENT_SHADER, fragmentShaderSource);
 
-    program = initShaders(gl);
+    // Link shaders into a program
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error(`Program linking error: ${gl.getProgramInfoLog(program)}`);
+        return;
+    }
+
     gl.useProgram(program);
+
+    // Create buffer and set vertices based on the parametrization
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    function createBreatherSurfaceVertices(uRange, vRange, uPrecision, vPrecision, aa) {
+        // Create an empty array to store the vertices.
+        const verticesBreathable = [];
+
+        // Loop over the u and v variables.
+        for (let u = uRange[0]; u <= uRange[1]; u += uPrecision) {
+            for (let v = vRange[0]; v <= vRange[1]; v += vPrecision) {
+                // Calculate the denominator.
+                const wsqr = 1 - aa * aa;
+                const w = Math.sqrt(wsqr);
+                const denom = aa * (Math.pow(w * Math.cosh(aa * u), 2) + Math.pow(aa * Math.sin(w * v), 2));
+
+                const x = -u + (2 * wsqr * Math.cosh(aa * u) * Math.sinh(aa * u) / denom);
+                const y = 2 * w * Math.cosh(aa * u) * (-(w * Math.cos(v) * Math.cos(w * v)) - (Math.sin(v) * Math.sin(w * v))) / denom;
+                const z = 2 * w * Math.cosh(aa * u) * (-(w * Math.sin(v) * Math.cos(w * v)) + (Math.cos(v) * Math.sin(w * v))) / denom;
+
+                // Add the vertex to the array.
+                verticesBreathable.push(vec4(x, y, z, 1.0));
+            }
+        }
+
+        // Return the array of vertices.
+        return verticesBreathable;
+    }
+
+
+    function generateBreatherShape(aa) {
+        function breatherSurface(u, v) {
+            const wsqr = 1 - aa * aa;
+            const w = Math.sqrt(wsqr);
+            const denom = aa * (Math.pow(w * Math.cosh(aa * u), 2) + Math.pow(aa * Math.sin(w * v), 2));
+
+            const x = -u + (2 * wsqr * Math.cosh(aa * u) * Math.sinh(aa * u) / denom);
+            const y = 2 * w * Math.cosh(aa * u) * (-(w * Math.cos(v) * Math.cos(w * v)) - (Math.sin(v) * Math.sin(w * v))) / denom;
+            const z = 2 * w * Math.cosh(aa * u) * (-(w * Math.sin(v) * Math.cos(w * v)) + (Math.cos(v) * Math.sin(w * v))) / denom;
+
+            return vec4(x, y, z, 1.0);
+        }
+
+        const verticesBreather = [];
+        const numPointsU = 1000;
+        const numPointsV = 1000;
+
+        for (let i = 0; i < numPointsU; i++) {
+            for (let j = 0; j < numPointsV; j++) {
+                const u = (i / (numPointsU - 1)) * 4 - 2; // Adjust the range as needed
+                const v = (j / (numPointsV - 1)) * Math.PI * 2;
+
+                const point = breatherSurface(u, v);
+                verticesBreather.push(point);
+            }
+        }
+
+        return verticesBreather;
+    }
+
+    const vertices = createBreatherSurfaceVertices(uRange, vRange, uPrecision, vPrecision, aa);
+    // <!-- var vertices = structuredClone(generateBreatherShape(0.76)); -->
+
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    // Get attribute location and enable it
+    const positionAttribLocation = gl.getAttribLocation(program, "a_position");
+    gl.vertexAttribPointer(positionAttribLocation, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionAttribLocation);
+
+    // Set the WebGL rendering context clear color
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.enable(gl.DEPTH_TEST);
 
-    modelViewMatrix = mat4();
+    // Set up the perspective matrix
+    var projectionMatrix = mat4();
+    projectionMatrix = perspective(Math.PI / 4, canvas.width / canvas.height, 0.1, 500);
 
-    modelViewMatrix = lookAt( vec3(0, 5, 5), vec3(0, 0, 0), vec3(0, 1, 0));
+    // Set up the model-view matrix
+    var modelViewMatrix = mat4();
+    modelViewMatrix = lookAt(vec3(0, 0, -400), vec3(0, 0, 0), vec3(0, 1, 0));
 
-    const uModelViewMatrix = gl.getUniformLocation(program, 'uModelViewMatrix');
-    gl.uniformMatrix4fv(uModelViewMatrix, false, flatten(modelViewMatrix));
+    var matrix = mat4();
+    matrix = mult(projectionMatrix, modelViewMatrix);
 
-    projectionMatrix = mat4();
+    // Set up the perspective matrix
+    const matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
-    projectionMatrix = perspective( radians(45), canvas.width / canvas.height, 0.1, 100.0);
+    gl.uniformMatrix4fv(matrixLocation, false, flatten(matrix));
 
-    const uProjectionMatrix = gl.getUniformLocation(program, 'uProjectionMatrix');
-    gl.uniformMatrix4fv(uProjectionMatrix, false, flatten(projectionMatrix));
-
-    const vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
-
-    const normalBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW);
-
-    const positionAttribute = gl.getAttribLocation(program, 'aVertexPosition');
-    gl.enableVertexAttribArray(positionAttribute);
-    gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
-
-    // const normalAttribute = gl.getAttribLocation(program, 'aVertexNormal');
-    // gl.enableVertexAttribArray(normalAttribute);
-    // gl.vertexAttribPointer(normalAttribute, 3, gl.FLOAT, false, 0, 0);
-
-    requestAnimationFrame(render);
-
-}
-
-let startTime;
-
-function render(timestamp) {
-    if (!startTime) {
-        startTime = timestamp;
-    }
-
-    const elapsedTime = (timestamp - startTime) / 1000.0;
-
-    // Pass the time value to the shader
-    const uTimeLocation = gl.getUniformLocation(program, 'uTime');
-    gl.uniform1f(uTimeLocation, elapsedTime);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, vertices.length / 3);
-
-    // Request the next animation frame
-    requestAnimationFrame(render);
-}
+    // Draw the vertices
+    gl.drawArrays(gl.POINTS, 0, vertices.length / 4);
+});
