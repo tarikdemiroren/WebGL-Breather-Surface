7a38ba94c97e885e6b65b78848d5aaf2cae13200
Tarık Demirören <tarekarez5652@gmail.com>
2023-12-17 22:09:32 +0300

    Gouraud Shading

--- breather.js
+++ breather.js
@@ -1,56 +1,52 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
 
     // Variables to track the initial mouse position
-let startMouseX = 0;
-let startMouseY = 0;
+    let startMouseX = 0;
+    let startMouseY = 0;
 
-// Variables to track the accumulated rotation angles
-let totalRotationX = 0;
-let totalRotationY = 0;
+    // Variables to track the accumulated rotation angles
+    let totalRotationX = 0;
+    let totalRotationY = 0;
 
-var isMouseDown = false;
+    var isMouseDown = false;
 
-// Event handlers
-function onMouseDown(event) {
-    isMouseDown = true;
+    // Event handlers
+    function onMouseDown(event) {
+        isMouseDown = true;
 
-    startMouseX = event.clientX;
-    startMouseY = event.clientY;
-}
-
-function onMouseMove(event) {
-
-    if(isMouseDown) {
-        const deltaX = event.clientX - canvas.getBoundingClientRect().left - startMouseX;
-        const deltaY = event.clientY - canvas.getBoundingClientRect().top - startMouseY;
-    
-        // Sensitivity factor to control rotation speed
-        const sensitivity = 0.2;
-    
-        // Calculate rotation angles
-        const rotationX = -deltaY * sensitivity;
-        const rotationY = -deltaX * sensitivity;
-    
-        // Update total rotation angles
-        totalRotationX = rotationX;
-        totalRotationY = rotationY;
-
-        console.log(totalRotationX, totalRotationY);
+        startMouseX = event.clientX;
+        startMouseY = event.clientY;
     }
-}
 
-function onMouseUp(event) {
-    isMouseDown = false;
-    totalRotationX = 0;
-    totalRotationY = 0;
-}
+    function onMouseMove(event) {
 
-// Attach event listeners
-document.addEventListener('mousedown', onMouseDown);
-document.addEventListener('mousemove', onMouseMove);
-document.addEventListener('mouseup', onMouseUp);
+        if (isMouseDown) {
+            const deltaX = event.clientX - canvas.getBoundingClientRect().left - startMouseX;
+            const deltaY = event.clientY - canvas.getBoundingClientRect().top - startMouseY;
+
+            // Sensitivity factor to control rotation speed
+            const sensitivity = 0.2;
+
+            // Calculate rotation angles
+            const rotationX = -deltaY * sensitivity;
+            const rotationY = -deltaX * sensitivity;
+
+            // Update total rotation angles
+            totalRotationX = rotationX;
+            totalRotationY = rotationY;
+        }
+    }
+
+    function onMouseUp(event) {
+        isMouseDown = false;
+        totalRotationX = 0;
+        totalRotationY = 0;
+    }
+
+    // Attach event listeners
+    document.addEventListener('mousedown', onMouseDown);
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
 
     let uRangeStart;
     let uRangeEnd;
@@ -59,10 +55,10 @@ document.addEventListener('mouseup', onMouseUp);
 
     let changed = false;
 
-    let uRange = [0, 16];
-    let vRange = [0, 23];
-    let uPrecision = 0.2;
-    let vPrecision = 0.2;
+    let uRange = [-16, 16];
+    let vRange = [-23, 23];
+    let uPrecision = 0.05;
+    let vPrecision = 0.05;
     let aa = 0.5;
 
     document.getElementById("button").addEventListener("click", getNumbers);
@@ -134,17 +130,64 @@ document.addEventListener('mouseup', onMouseUp);
 
     // Vertex and fragment shader source code
     const vertexShaderSource = `
-        attribute vec4 a_position;
-        uniform mat4 u_matrix;
+        attribute vec4 vPosition;
+        attribute vec4 vNormal;
+
+        varying vec4 fColor;
+
+        uniform vec4 ambientProduct, diffuseProduct, specularProduct;
+        uniform float shininess;
+        uniform mat4 modelViewMatrix;
+        uniform mat4 projectionMatrix;
+        uniform vec4 lightPosition;
+        uniform mat3 normalMatrix;
+
         void main() {
-            gl_Position = u_matrix * a_position;
+            vec3 pos = -(modelViewMatrix * vPosition).xyz;
+
+            vec3 L;
+
+            if (lightPosition.w == 0.0){
+                L = normalize(lightPosition.xyz);
+            }
+            else {
+                L = normalize(lightPosition.xyz - pos);
+            }
+
+            vec3 E = normalize( -pos );
+            vec3 H = normalize(L + E);
+            vec3 N = normalize( normalMatrix * vNormal.xyz );
+
+            //float ambientOcclusion = max(dot(N, E), 0.2);
+            vec4 ambient = ambientProduct; //* ambientOcclusion;
+
+            float Kd = max( dot(L, N), 0.0);
+            vec4 diffuse = Kd * diffuseProduct;
+
+            float Ks = pow( max( dot(N, H), 0.0), shininess);
+            //float Ks = smoothstep(0.0, 1.0, pow(max(dot(N, H), 0.0), shininess));
+            vec4 specular = Ks * specularProduct;
+
+            if ( dot( L , N ) < 0.0 ){
+                specular = vec4(0.0, 0.0, 0.0, 1.0);
+            }
+
+            fColor = ambient + diffuse + specular;
+
+            fColor.a = 1.0;
+
+            gl_Position = projectionMatrix * modelViewMatrix * vPosition;
         }
     `;
 
     const fragmentShaderSource = `
         precision mediump float;
+
+        varying vec4 fColor;
+
         void main() {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
+            
+            gl_FragColor = fColor;
         }
     `;
 
@@ -177,27 +220,51 @@ document.addEventListener('mouseup', onMouseUp);
         return;
     }
 
+    gl.enable(gl.DEPTH_TEST);
+
     gl.useProgram(program);
 
-    // Create buffer and set vertices based on the parametrization
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    let normalsArray = [];
 
-    function quad(pointsArray, a, b, c, d) {
-        var t1 = subtract(b, a);
-        var t2 = subtract(c, b);
-        var normal = cross(t1, t2);
-        var normal = vec3(normal);
-      
-        pointsArray.push(a);
-        // normalsArray.push(normal);
-        pointsArray.push(b);
-        // normalsArray.push(normal);
-        pointsArray.push(c);
-        // normalsArray.push(normal);
-        pointsArray.push(d);
-        // normalsArray.push(normal);
-      }
+    function reverseNormal(normal) {
+        return vec4(-normal[0], -normal[1], -normal[2], 0.0);
+    }
+
+    function calculateNormals(vertices) {
+        var normals = [];
+    
+        for (var i = 0; i < vertices.length - 2; i += 3) {
+            var v0 = vertices[i];
+            var v1 = vertices[i + 1];
+            var v2 = vertices[i + 2];
+    
+            var edge1 = subtract(v1, v0);
+            var edge2 = subtract(v2, v0);
+            var normal = normalize(cross(edge1, edge2));
+    
+            normals.push(normal, normal, normal);
+        }
+    
+        var averagedNormals = [];
+    
+        for (var i = 0; i < vertices.length; i++) {
+            var normalSum = vec3(0, 0, 0);
+    
+            // Sum normals from adjacent triangles
+            for (var j = 0; j < normals.length; j += 3) {
+                if (Math.floor(i / 3) === Math.floor(j / 3)) {
+                    normalSum = add(normalSum, normals[j]);
+                }
+            }
+    
+            // Average and normalize the sum
+            var averagedNormal = normalize(normalSum);
+            averagedNormals.push(averagedNormal);
+        }
+    
+        return averagedNormals;
+    }
+    
 
     function createBreatherSurfaceVertices(uRange, vRange, uPrecision, vPrecision, aa) {
         // Create an empty array to store the vertices.
@@ -205,6 +272,10 @@ document.addEventListener('mouseup', onMouseUp);
         const verticesBreathable2 = [];
 
         const totalVertices = [];
+        const normalVertices = [];
+
+        const sebvertices = [];
+        const sebnormals = [];
         var chose = 1;
 
         for (let u = uRange[0]; u <= uRange[1]; u += uPrecision) {
@@ -217,52 +288,140 @@ document.addEventListener('mouseup', onMouseUp);
                 const y = 2 * w * Math.cosh(aa * u) * (-(w * Math.cos(v) * Math.cos(w * v)) - (Math.sin(v) * Math.sin(w * v))) / denom;
                 const z = 2 * w * Math.cosh(aa * u) * (-(w * Math.sin(v) * Math.cos(w * v)) + (Math.cos(v) * Math.sin(w * v))) / denom;
 
-                if (chose % 3 == 1){
+                if (chose % 3 == 1) {
                     verticesBreathable1.push(vec4(x, y, z, 1.0));
                 }
-                else if (chose % 3 == 2){
+                else if (chose % 3 == 2) {
                     verticesBreathable2.push(vec4(x, y, z, 1.0));
                 }
             }
             chose++;
-            if (chose % 3 == 0){
-                u -= vPrecision;
-                if (length_of_strip === undefined){
+            if (chose % 3 == 0) {
+                u -= uPrecision;
+                if (length_of_strip === undefined) {
                     length_of_strip = verticesBreathable1.length;
                 }
-                // else if (u != uRange[1] - uPrecision) {
-                //     u -= vPrecision;
-                //     // for (var j = 0; j < length_of_strip; j++){
-                //     //     var k = verticesBreathable1[j];
-                //     //     var l = totalVertices[ (totalVertices.length - (2*length_of_strip)) + j + (j + 1) ]
-                //     //     totalVertices.push(k);
-                //     //     totalVertices.push(l);
-                //     // }
-                // }
-                for (var i = 0; i < verticesBreathable1.length; i++){
+                for (var i = 0; i < verticesBreathable1.length; i++) {
                     var a = verticesBreathable1.shift();
                     var b = verticesBreathable2.shift();
-                    totalVertices.push( a );
-                    totalVertices.push( b );
+                    totalVertices.push(a);
+                    totalVertices.push(b);
                 }
                 chose++;
             }
         }
-        length_of_strip = undefined;
 
-        return totalVertices;
+        function quad(a, b, c, d) {
+            var t1 = subtract(totalVertices[b], totalVertices[a]);
+            var t2 = subtract(totalVertices[c], totalVertices[b]);
+            var normali = cross(t1, t2);
+            var normal = vec3(normali);
+          
+            sebvertices.push(totalVertices[a]);
+            normalVertices.push(normal);
+            sebvertices.push(totalVertices[b]);
+            normalVertices.push(normal);
+            sebvertices.push(totalVertices[c]);
+            normalVertices.push(normal);
+            sebvertices.push(totalVertices[a]);
+            normalVertices.push(normal);
+            sebvertices.push(totalVertices[c]);
+            normalVertices.push(normal);
+            sebvertices.push(totalVertices[d]);
+            normalVertices.push(normal);
+          }
+
+
+        for (var i = 0; i < totalVertices.length - 3; i = i + 2) {
+
+            //quad(i, i+1, i+2, i+3);
+            sebvertices.push(totalVertices[i]);     // a
+            sebvertices.push(totalVertices[i + 1]); // b
+            sebvertices.push(totalVertices[i + 2]); // c - quad d
+            sebvertices.push(totalVertices[i + 2]); // c - quad d
+            sebvertices.push(totalVertices[i + 1]); // b
+            sebvertices.push(totalVertices[i + 3]); // d - quad c
+
+
+
+            var t1 = subtract(totalVertices[i + 1], totalVertices[i]); // b a
+            var t2 = subtract(totalVertices[i + 2], totalVertices[i]); // c a
+            var normal = normalize(cross(t2, t1));
+            normal = vec4(normal);
+            normal[3] = 0.0;
+
+            sebnormals.push(normal);
+            sebnormals.push(normal);
+            sebnormals.push(normal);
+
+            var t1 = subtract(totalVertices[i + 1], totalVertices[i + 2]); // b a
+            var t2 = subtract(totalVertices[i + 3], totalVertices[i + 2]); // c a
+            var normal = normalize(cross(t2, t1));
+            normal = vec4(normal);
+            normal[3] = 0.0;
+
+            sebnormals.push(normal);
+            sebnormals.push(normal);
+            sebnormals.push(normal);
+            
+        }
+
+
+        //length_of_strip = undefined;
+
+        // chose = 0;
+
+        // for (var i = 0; i < totalVertices.length - 2; i++) {
+        //     var t1 = subtract(totalVertices[i + 1], totalVertices[i]);
+        //     var t2 = subtract(totalVertices[i + 2], totalVertices[i]);
+        //     var normal = normalize(cross(t2, t1));
+
+        //     normal = vec4(normal);
+        //     normal[3] = 0.0;
+
+        //     if (i % (length_of_strip) == 0){
+        //         chose = 0;
+        //     }
+
+        //     if (chose % 2 == 0){
+        //         normal = reverseNormal(normal);
+        //     }
+        //     chose++;
+
+        //     normalVertices.push(normal);
+        //     normalVertices.push(normal);
+        //     normalVertices.push(normal);
+
+        //     // normalVertices.push(totalVertices[i][0], totalVertices[i][1], totalVertices[i][2], 0.0 );
+        //     // normalVertices.push(totalVertices[i+1][0], totalVertices[i+1][1], totalVertices[i+1][2], 0.0 );
+        //     // normalVertices.push(totalVertices[i+2][0], totalVertices[i+2][1], totalVertices[i+2][2], 0.0 );
+        // }
+
+        normalsArray = (sebnormals);
+
+
+
+        return sebvertices;
     }
 
     let vertices = createBreatherSurfaceVertices(uRange, vRange, uPrecision, vPrecision, aa);
 
+    // Create buffer and set vertices based on the parametrization
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
-    // Get attribute location and enable it
-    const positionAttribLocation = gl.getAttribLocation(program, "a_position");
+    const positionAttribLocation = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(positionAttribLocation, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(positionAttribLocation);
 
-    gl.enable(gl.DEPTH_TEST);
+    const nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+    const vNormal = gl.getAttribLocation(program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
 
     // Set up the perspective matrix
     var projectionMatrix = mat4();
@@ -272,13 +431,50 @@ document.addEventListener('mouseup', onMouseUp);
     var modelViewMatrix = mat4();
     modelViewMatrix = lookAt(vec3(0, 0, -(zoomCoefficient)), vec3(0, 0, 0), vec3(0, 1, 0));
 
-    var matrix = mat4();
-    matrix = mult(projectionMatrix, modelViewMatrix);
-
     // Set up the perspective matrix
-    var matrixLocation = gl.getUniformLocation(program, "u_matrix");
+    var modelviewmatrixLocation = gl.getUniformLocation(program, "modelViewMatrix");
 
-    gl.uniformMatrix4fv(matrixLocation, false, flatten(matrix));
+    gl.uniformMatrix4fv(modelviewmatrixLocation, false, flatten(modelViewMatrix));
+
+    var projectionmatrixLocation = gl.getUniformLocation(program, "projectionMatrix");
+
+    gl.uniformMatrix4fv(projectionmatrixLocation, false, flatten(projectionMatrix));
+
+    var normalMatrixLocation = gl.getUniformLocation(program, "normalMatrix");
+    
+    normalMatrix = [
+        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+    ];
+
+    gl.uniformMatrix3fv(normalMatrixLocation, false, flatten(normalMatrix));
+
+    var lightPosition = vec4(0.0, 0.0, -60.0, 0.0 );
+    var lightAmbient = vec4(0.1, 0.1, 0.1, 1.0 );
+    var lightDiffuse = vec4( 0.5, 0.5, 0.5, 1.0 );
+    var lightSpecular = vec4( 0.3, 0.3, 0.3, 1.0 );
+    
+    var materialAmbient = vec4( 0.0, 0.0, 1.0, 1.0 );
+    var materialDiffuse = vec4( 0.0, 0.0, 1.0, 1.0 );
+    var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+    var materialShininess = 4.0;
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+    var lightPositionLoc = gl.getUniformLocation(program, "lightPosition");
+    var ambientColorLoc = gl.getUniformLocation(program, "ambientProduct");
+    var diffuseColorLoc = gl.getUniformLocation(program, "diffuseProduct");
+    var specularColorLoc = gl.getUniformLocation(program, "specularProduct");
+    var shininessLoc = gl.getUniformLocation(program, "shininess");
+
+    gl.uniform4fv(lightPositionLoc, flatten(lightPosition));
+    gl.uniform4fv(ambientColorLoc, flatten(ambientProduct));
+    gl.uniform4fv(diffuseColorLoc, flatten(diffuseProduct));
+    gl.uniform4fv(specularColorLoc, flatten(specularProduct));
+    gl.uniform1f(shininessLoc, materialShininess);
 
     render();
 
@@ -286,13 +482,16 @@ document.addEventListener('mouseup', onMouseUp);
         // Set the WebGL rendering context clear color
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         // Clear the color buffer with specified clear color
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         // console.log(`uRange: ${uRange} vRange: ${vRange}, uPrecision: ${uPrecision} vPrecision: ${vPrecision} aa: ${aa}`);
 
         if (changed) {
-            vertices = structuredClone(createBreatherSurfaceVertices(uRange, vRange, uPrecision, vPrecision, aa));
+            vertices = createBreatherSurfaceVertices(uRange, vRange, uPrecision, vPrecision, aa);
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+            gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
         }
 
         // modelViewMatrix = mult(modelViewMatrix, rotate(totalRotationX * (Math.PI / 180), [1, 0, 0]));
@@ -300,13 +499,16 @@ document.addEventListener('mouseup', onMouseUp);
         modelViewMatrix[2][3] = -(zoomCoefficient);
         modelViewMatrix = mult(modelViewMatrix, rotate(totalRotationY * (Math.PI / 180), [0, 1, 0]));
 
-        matrix = mult(projectionMatrix, modelViewMatrix);
+        //var modelviewmatrixLocation = gl.getUniformLocation(program, "modelViewMatrix");
 
-        matrixLocation = gl.getUniformLocation(program, "u_matrix");
-        gl.uniformMatrix4fv(matrixLocation, false, flatten(matrix));
+        gl.uniformMatrix4fv(modelviewmatrixLocation, false, flatten(modelViewMatrix));
+
+        // var projectionmatrixLocation = gl.getUniformLocation(program, "projectionMatrix");
+
+        // gl.uniformMatrix4fv(projectionmatrixLocation, false, flatten(projectionMatrix));
 
         changed = false;
-        gl.drawArrays(gl.LINES, 0, vertices.length );
+        gl.drawArrays(gl.TRIANGLES, 0, vertices.length);
         requestAnimationFrame(render)
     }
 });
